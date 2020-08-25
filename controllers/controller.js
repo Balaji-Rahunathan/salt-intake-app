65123852c63054c2080f658e38693f6d598bc86a
Balaji <mynameisbalag@gmail.com>
2020-08-25 09:35:27 +0530

    Desktop design completed

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,24 +4,47 @@ function getCurrentId(event) {
     if (!dragging) {
         currentId = event.target.id
     }
+}
 
-    $("#" + currentId).draggable({
+var url = "../assets/data/product.json";         
+$.getJSON(url, function (data) {
+    $.each(data, function (key, model) {
+        console.log(model._id) 
+        var rack = document.getElementById("rack-container")
+        var productContainer = document.createElement("DIV")
+        productContainer.classList.add("product-container");
+        productContainer.classList.add("container-draggable"+model._id);
+        var product =  document.createElement("IMG")
+        product.classList.add("product")
+        product.classList.add("draggable"+model._id)
+        product.id = "draggable"+model._id
+        product.setAttribute("draggable", true)
+        product.setAttribute("onmouseover", "getCurrentId(event)")
+        product.setAttribute("ontouchstart", "getCurrentId(event)")
+        product.setAttribute("saltLevel", model.saltLevel)
+        product.setAttribute("riskFactor", model.riskFactor)
+        product.setAttribute("image", model.image)
+        product.setAttribute("name", model.name)
+        product.src = model.image
+        productContainer.appendChild(product)
+        rack.appendChild(productContainer)
+
+    $("#" + "draggable"+model._id).draggable({
         scroll: false,
-        cursor: "move",
+        // cursor: "move",
         start: function (event) {
             var stylesheet = document.styleSheets[0]
-            stylesheet.insertRule(`.${currentId} { top:0px !important; left:0px !important;}`, 0);
+            stylesheet.insertRule(`.${"draggable"+model._id} { top:0px !important; left:0px !important;}`, 0);
         },
         stop: function (event, ui) {
             var x = event.clientX;
             var y = event.clientY;
             elementMouseIsOver = document.elementFromPoint(x, y);
-            
             if (elementMouseIsOver.id !== "drop-target") {
-                var container = document.getElementsByClassName("container-" + currentId);
-                container[0].appendChild(document.getElementById(currentId))
+                var container = document.getElementsByClassName("container-" + "draggable"+model._id);
+                container[0].appendChild(document.getElementById("draggable"+model._id))
             }
-            var target = document.getElementById(currentId);
+            var target = document.getElementById("draggable"+model._id);
             target.style.transform = `translate(0px,0px)`
             target.style.top = 0
             target.style.left = 0
@@ -32,8 +55,8 @@ function getCurrentId(event) {
         },
         drag: function (event, ui) {
             var target = document.getElementById("page");
-            target.appendChild(document.getElementById(currentId))
-            var product = document.getElementsByClassName(currentId)
+            target.appendChild(document.getElementById("draggable"+model._id))
+            var product = document.getElementsByClassName("draggable"+model._id)
             product[0].style.transform = `translate(${event.clientX - product[0].offsetWidth / 2}px,${event.clientY - product[0].offsetHeight / 2}px)`
             dragging = true
         }
@@ -43,11 +66,72 @@ function getCurrentId(event) {
     $("#drop-target").droppable({
         drop: function (event, ui) {
             var target = document.getElementById(currentId);
+            var child = document.getElementById("drop-target").firstChild
+            var name = document.getElementById("name") 
+            var image = document.getElementById("productImage") 
+            var riskFactor = document.getElementById("riskFactor") 
+            var saltLevel = document.getElementById("saltLevel") 
+            saltLevel.innerHTML =  target.getAttribute("saltLevel")
+            riskFactor.innerHTML =  target.getAttribute("riskFactor")
+            image.src =  target.getAttribute("image")
+            name.innerHTML =  target.getAttribute("name")
+            event.target.removeChild(child)
             event.target.appendChild(target);
+            target.style.display = "none"
             target.style.top = 0
             target.style.left = 0
             target.style.right = 0
             target.style.bottom = 0
         }
     });
-}
+
+})
+});
+
+
+// $("#" + currentId).draggable({
+//     scroll: false,
+//     cursor: "move",
+//     start: function (event) {
+//         var stylesheet = document.styleSheets[0]
+//         stylesheet.insertRule(`.${currentId} { top:0px !important; left:0px !important;}`, 0);
+//     },
+//     stop: function (event, ui) {
+//         var x = event.clientX;
+//         var y = event.clientY;
+//         elementMouseIsOver = document.elementFromPoint(x, y);
+
+//         if (elementMouseIsOver.id !== "drop-target") {
+//             var container = document.getElementsByClassName("container-" + currentId);
+//             container[0].appendChild(document.getElementById(currentId))
+//         }
+//         var target = document.getElementById(currentId);
+//         target.style.transform = `translate(0px,0px)`
+//         target.style.top = 0
+//         target.style.left = 0
+//         target.style.right = 0
+//         target.style.bottom = 0
+//         target.style.opacity = 1
+//         dragging = false
+//     },
+//     drag: function (event, ui) {
+//         var target = document.getElementById("page");
+//         target.appendChild(document.getElementById(currentId))
+//         var product = document.getElementsByClassName(currentId)
+//         product[0].style.transform = `translate(${event.clientX - product[0].offsetWidth / 2}px,${event.clientY - product[0].offsetHeight / 2}px)`
+//         dragging = true
+//     }
+// });
+
+
+// $("#drop-target").droppable({
+//     drop: function (event, ui) {
+//         var target = document.getElementById(currentId);
+//         event.target.appendChild(target);
+//         target.style.top = 0
+//         target.style.left = 0
+//         target.style.right = 0
+//         target.style.bottom = 0
+//     }
+// });
+
